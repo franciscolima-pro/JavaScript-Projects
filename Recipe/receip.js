ab8cea41899a5bae24bfae69ac069338bf10d115
Francisco Lima <franciscolimapro@gmail.com>
2024-08-04 16:05:20 -0300

    added recipe

---
+++ Recipe/receip.js
@@ -0,0 +1,12 @@
+document.getElementById('toggle').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  const icon = this.querySelector('i');
+  if (document.body.classList.contains('dark-mode'))
+  {
+    icon.classList.add('fa-sun');
+    icon.classList.remove('fa-moon')
+  }else{
+    icon.classList.add('fa-moon')
+    icon.classList.remove('fa-sun');
+  }
+});
